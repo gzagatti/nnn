c10934808b136d2c580ad3e4408bd7d12d6bc4c0
gzagatti <gzagatti@users.noreply.github.com>
2022-02-21 10:26:26 +0800

    allow filtering hidden lists.

--- src/nnn.c
+++ src/nnn.c
@@ -7155,8 +7155,8 @@ nochange:
 			add_bookmark(path, newpath, &presel);
 			goto nochange;
 		case SEL_FLTR:
-			if (!ndents)
-				goto nochange;
+			/* if (!ndents)
+				goto nochange; */
 			/* Unwatch dir if we are still in a filtered view */
 #ifdef LINUX_INOTIFY
 			if (inotify_wd >= 0) {
